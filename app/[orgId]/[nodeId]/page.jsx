2943b4f2bb2b39d388c4bd4b55ba63b49049107e
gazdagergo <info@gazdagergo.com>
2024-06-19 15:31:29 +0200

    initial commit

---
+++ app/[orgId]/[nodeId]/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const NodePage = () => {
+  return (
+    <div>
+      <Image width="1000" height="500" src="/FSZ135_FSZ128.jpg" alt="arrow towards 128" />
+    </div>
+  )
+}
+
+export default NodePage
