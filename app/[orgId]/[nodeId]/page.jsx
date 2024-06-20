6effd5579506f6ef72d938d095bba98b18d7a6fb
gazdagergo <info@gazdagergo.com>
2024-06-20 14:16:45 +0200

    add navigator

--- app/[orgId]/[nodeId]/page.jsx
+++ app/[orgId]/[nodeId]/page.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navigator from "@/components/navigator";
 
 const NodePage = () => {
   return (
     <div>
-      <Image width="1000" height="500" src="/FSZ135_FSZ128.jpg" alt="arrow towards 128" />
+      <Navigator  width="1000" height="500" src="/FSZ135_FSZ128.jpg" alt="arrow towards 128" />
     </div>
   )
 }
